b1935c41b6d14f0682bc258ef9193174920dfc0e
mudaldosa <bms9849@example.com>
2026-04-03 00:52:11 +0900

    final

--- todo.py
+++ todo.py
@@ -1,6 +1,5 @@
-# 리스트를 각각 관리하면 설명하기 훨씬 쉽습니다!
-todos = []
-priorities = [] # 중요도를 따로 저장하는 리스트
+todos = [] # 할일 저장하는 리스트
+priorities = [] # 중요도 저장하는 리스트
 
 while True:
     print("\n[나의 할 일 관리자]")
@@ -10,7 +9,7 @@ while True:
 
     if choice == "1":
         todo = input("할 일: ")
-        # 중요도를 물어보고 바로 리스트에 넣기
+       
         p = input("중요한가요? (별표 표시하려면 1 입력): ")
         
         todos.append(todo)
@@ -25,23 +24,23 @@ while True:
             print("목록이 비어있어요.")
         else:
             for i in range(len(todos)):
-                # 두 리스트에서 같은 번호를 꺼내서 보여주기
+               
                 print(f"{i+1}. [{priorities[i]}] {todos[i]}")
 
-    elif choice == "3": # 수정 기능
+    elif choice == "3": 
         idx = int(input("수정할 번호: ")) - 1
         if 0 <= idx < len(todos):
             new_text = input("수정할 내용: ")
-            todos[idx] = new_text # 리스트의 특정 위치 값만 바꾸기
+            todos[idx] = new_text # 리스트의 특정 위치 값만 수정
             print("수정되었습니다.")
         else:
             print("번호를 확인해주세요.")
 
-    elif choice == "4": # 삭제 기능
+    elif choice == "4": 
         idx = int(input("삭제할 번호: ")) - 1
         if 0 <= idx < len(todos):
             todos.pop(idx)
-            priorities.pop(idx) # 중요도 리스트도 같이 삭제해줘야 순서가 맞음!
+            priorities.pop(idx) # pop 이용 삭제 기능
             print("삭제 완료.")
 
     elif choice == "5":
